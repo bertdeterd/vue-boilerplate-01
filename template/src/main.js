15f3e32a2efd80d0e471aca53c5a3138e8360a83
bertdeterd <bertdeterd@creetion.com>
2017-11-22 15:08:53 +0100

    changes

--- template/src/main.js
+++ template/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 
+import axios from 'axios'
+
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios.create();
 
 
 /* eslint-disable no-new */
