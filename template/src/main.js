1328059c17dd42dd1a2d250bfcd42e96c26a4e31
bertdeterd <bertdeterd@creetion.com>
2017-11-23 08:47:33 +0100

    changes

--- template/src/main.js
+++ template/src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App'
 import axios from 'axios'
 import router from './router'
+import Vuetify from 'vuetify'
+
+import 'vuetify/dist/vuetify.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
