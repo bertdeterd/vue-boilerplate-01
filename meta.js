80d08a0cae124d80fc58d5f1c1b067973c918462
bertdeterd <bertdeterd@creetion.com>
2017-11-26 20:57:05 +0100

    changes

--- meta.js
+++ meta.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "helpers": {
-    "if_or": function (v1, v2, options) {
+  helpers: {
+    if_or: function(v1, v2, options) {
       if (v1 || v2) {
         return options.fn(this);
       }
@@ -9,31 +9,35 @@ module.exports = {
     }
   },
 
-  "prompts": {
-    "name": {
-      "type": "string",
-      "required": true,
-      "message": "Project naam"
+  prompts: {
+    name: {
+      type: "string",
+      required: true,
+      message: "Project naam"
     },
-    "description": {
-      "type": "string",
-      "required": true,
-      "message": "Project beschrijving",
-      "default": "Vue.js template project"
+    description: {
+      type: "string",
+      required: true,
+      message: "Project beschrijving",
+      default: "Vue.js template project"
     },
-    "author": {
-      "type": "string",
-      "message": "Developer",
-      "default": "Creetion"
+    author: {
+      type: "string",
+      message: "Developer",
+      default: "Creetion"
     },
-    "applicationtitle": {
-      "type": "string",
-      "message": "Applicatie header",
-      "default": "Demo"
+    applicationtitle: {
+      type: "string",
+      message: "Applicatie header",
+      default: "Demo"
+    },
+    applicationtheme: {
+      type: "list",
+      message: "What theme to use as base?",
+      choices: "['dark','light']"
     }
   },
-  "filters": {
-    
-  },
-  "completeMessage": "Nabewerkingen:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\n"
+  filters: {},
+  completeMessage:
+    "Nabewerkingen:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\n"
 };
