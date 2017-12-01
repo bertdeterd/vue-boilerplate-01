8347fb847f0f022c1fd183002d478c9cf3e94b1b
bertdeterd <bertdeterd@creetion.com>
2017-12-01 20:21:06 +0100

    changes

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -1,63 +1,29 @@
 <template>
-  <v-data-table
-      v-model="selected"
+   <v-data-table
       v-bind:headers="headers"
-      v-bind:items="items"
-      select-all
-      v-bind:pagination.sync="pagination"
-      item-key="name"
-      class="elevation-1"
+      :items="items"
+      hide-actions
+      class="elevation-1"  
     >
-    <template slot="headers" slot-scope="props">
-      <tr>
-        <th>
-          <v-checkbox
-            primary
-            hide-details
-            @click.native="toggleAll"
-            :input-value="props.all"
-            :indeterminate="props.indeterminate"
-          ></v-checkbox>
-        </th>
-        <th v-for="header in props.headers" :key="header.text"
-          :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
-          @click="changeSort(header.value)"
-        >
-          <v-icon>arrow_upward</v-icon>
-          {{ header.text }}
-        </th>
-      </tr>
-    </template>
     <template slot="items" slot-scope="props">
-      <tr :active="props.selected" @click="props.selected = !props.selected">
-        <td>
-          <v-checkbox
-            primary
-            hide-details
-            :input-value="props.selected"
-          ></v-checkbox>
-        </td>
-        <td>{{ props.item.name }}</td>
-        <td class="text-xs-right">{{ props.item.calories }}</td>
-        <td class="text-xs-right">{{ props.item.fat }}</td>
-        <td class="text-xs-right">{{ props.item.carbs }}</td>
-        <td class="text-xs-right">{{ props.item.protein }}</td>
-        <td class="text-xs-right">{{ props.item.sodium }}</td>
-        <td class="text-xs-right">{{ props.item.calcium }}</td>
-        <td class="text-xs-right">{{ props.item.iron }}</td>
-      </tr>
+      <td>{{ props.item.name }}</td>
+      <td class="text-xs-right">{{ props.item.calories }}</td>
+      <td class="text-xs-right">{{ props.item.fat }}</td>
+      <td class="text-xs-right">{{ props.item.carbs }}</td>
+      <td class="text-xs-right">{{ props.item.protein }}</td>
+      <td class="text-xs-right">{{ props.item.sodium }}</td>
+      <td class="text-xs-right">{{ props.item.calcium }}</td>
+      <td class="text-xs-right">{{ props.item.iron }}</td>
     </template>
   </v-data-table>
 </template>
-
-
 
 <script>
 export default {
   name: "HelloWorld",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App",
+      msg: "Welcomexx to Your Vue.js App",
       selected: [],
       pagination: {
         sortBy: "name"
